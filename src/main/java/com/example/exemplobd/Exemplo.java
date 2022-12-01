01db8c80360563fbb3c5ff4dec043048145c6c09
Dhiulia da Silva <hdn.dhiulia@voeazul.com.br>
2022-11-30 22:52:49 -0300

    excel report

--- src/main/java/com/example/exemplobd/Exemplo.java
+++ src/main/java/com/example/exemplobd/Exemplo.java
@@ -6,11 +6,24 @@
 package com.example.exemplobd;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 
 /**
  *
@@ -31,9 +44,62 @@ public class Exemplo extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         launch(args);
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Dados dos Pets");
+        Dao_Pet pet = new Dao_Pet();
+        var pets = pet.pesquisaTodos();
+
+        Map<Integer, Object[]> data = new TreeMap();
+        data.put(0, new Object[]{"ID", "NOME", "TIPO", "SEXO", "IDADE", "RAÇA", "PESO", "DONO"});
+        for (Pet p : pets) {
+            data.put(p.getId(), new Object[]{
+                    p.getId(),
+                    p.getNome(),
+                    p.getTipo(),
+                    p.getSexo(),
+                    p.getIdade(),
+                    p.getRaca(),
+                    p.getPeso(),
+                    p.getId_dono()});
+        }
+
+        Set<Integer> keyset = data.keySet();
+        int rownum = 0;
+        Iterator var6 = keyset.iterator();
+
+        while (var6.hasNext()) {
+            Integer key = (Integer) var6.next();
+            Row row = sheet.createRow(rownum++);
+            Object[] objArr = (Object[]) data.get(key);
+            int cellnum = 0;
+            Object[] var11 = objArr;
+            int var12 = objArr.length;
+
+            for (int var13 = 0; var13 < var12; ++var13) {
+                Object obj = var11[var13];
+                Cell cell = row.createCell(cellnum++);
+                if (obj instanceof String) {
+                    cell.setCellValue((String) obj);
+                } else if (obj instanceof Integer) {
+                    cell.setCellValue((Integer) obj);
+                } else if (obj instanceof Float) {
+                    cell.setCellValue((Float) obj);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream(new File("PetsCadastrados.xls"));
+            workbook.write(out);
+            out.close();
+        } catch (Exception var16) {
+            var16.printStackTrace();
+        }
     }
-
-
 }
+
+
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.exemplobd {
 
     requires org.controlsfx.controls;
     requires java.sql;
+    requires poi;
 
     opens com.example.exemplobd to javafx.fxml;
     exports com.example.exemplobd;
